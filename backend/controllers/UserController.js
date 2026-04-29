5bf71a58b69354ee48fd3f4f6518855f55b159db
GuilhermePedroso <guiwpedroso@gmail.com>
2026-04-29 12:42:47 -0300

    Aula29-04 Liskov-Verificação-de-token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,49 +1,52 @@
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const createUserToken = require('../helpers/create-user-token')
+const getToken = require('../helpers/get-tokens')
+
 
 module.exports = class UserController {
     static async register(req, res) {
         const { name, email, phone, password, confirmpassword } = req.body
 
         if (!name) {
-            res.status(422).json({ message: 'O nome é obrigatório' })
+            res.status(422).json({ message: 'Nome é obrigatório'})
             return
         }
 
         if (!email) {
-            res.status(422).json({ message: 'O email é obrigatório' })
+            res.status(422).json({ message: 'Email é obrigatório'})
             return
         }
 
         if (!phone) {
-            res.status(422).json({ message: 'O telefone é obrigatório' })
+            res.status(422).json({ message: 'Telefone é obrigatório'})
             return
         }
 
         if (!password) {
-            res.status(422).json({ message: 'A senha é obrigatória' })
+            res.status(422).json({ message: 'Senha é obrigatório'})
             return
         }
 
         if (!confirmpassword) {
-            res.status(422).json({ message: 'A confirmação de senha é obrigatória' })
+            res.status(422).json({ message: 'Confirmação de senha é obrigatório'})
             return
         }
 
         if (password !== confirmpassword) {
-            res.status(422).json({ message: 'As senhas não conferem' })
+            res.status(422).json({ message: 'As senhas não coincidem'})
             return
         }
 
-        const userExists = await User.findOne({ email: email })
+        const userExists = await User.findOne({ email:email})
 
         if (userExists) {
-            res.status(422).json({ message: 'O usuário já existe em nossos registros!' })
+            res.status(422).json({ message: 'O usuário já existe em nossos registros.'})
             return
         }
 
-        const salt = await bcrypt.genSalt(10)
+        const salt = await bcrypt.genSalt(12)
         const passwordHash = await bcrypt.hash(password, salt)
 
         const user = new User({
@@ -52,43 +55,81 @@ module.exports = class UserController {
             phone,
             password: passwordHash,
         })
-
+       
         try {
             const newUser = await user.save()
             await createUserToken(newUser, req, res)
-
         } catch (error) {
-            res.status(500).json({ message: error })
+            res.status(503).json({ message: error })
         }
-
     }
+
     static async login(req, res) {
         const { email, password } = req.body
 
         if (!email) {
-            res.status(422).json({ message: 'O email é obrigatório' })
+            res.status(422).json({ message: 'Email é obrigatório'})
             return
         }
-
         if (!password) {
-            res.status(422).json({ message: 'A senha é obrigatória' })
+            res.status(422).json({ message: 'Senha é obrigatório'})
             return
         }
-
         const userExists = await User.findOne({ email: email })
 
         if (!userExists) {
-            res.status(401).json({ message: 'Não autorizado, sem registro!'})
+            res.status(401).json({
+                message: 'Não autorizado, sem registro'
+            })
             return
         }
 
         const checkPassword = await bcrypt.compare(password, userExists.password)
 
         if (!checkPassword) {
-            res.status(401).json({ message: 'Não autorizado, sem registro!' })
+            res.status(401).json({
+                message: 'Não autorizado, sem registro'
+            })
             return
         }
 
-        await createUserToken(user, req, res)
-    }   
+        await createUserToken(userExists, req, res)
+    }
+
+    static async checkUser(req, res){
+        let currentUser
+        console.log(req.headers.authorization)
+
+        if(req.headers.authorization){
+            const token = getToken(req)
+            const decoded = jwt.verify(token, 'fatec-turma6-a2026')
+
+            currentUser = await User.findById(decoded.id)
+            currentUser.password = undefined
+        }else{
+            currentUser = null
+        }
+
+        res.status(200).send(currentUser)
+    }
+
+    static async getUserById(req, res){
+        const id = req.params.id
+
+        const user = await User.findById(id)
+
+        if(!user){
+            res.status(404).json({
+                message: 'Usuário não encontrado'
+            })
+            return
+        }
+
+        res.status(200).json(user)
+    }
+    static async editUser(req, res){
+     res.status(200).json({
+    message: 'Usuário atualizado com sucesso'   
+     })
+    }
 }
